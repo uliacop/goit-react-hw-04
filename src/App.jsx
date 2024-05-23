e5ddca126c6ad4713e63f043f6a7f84a3929a174
uliacop <115625925+uliacop@users.noreply.github.com>
2024-05-24 00:20:25 +0300

    delete comment code

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,3 @@
-/* import { useState, useEffect } from "react";
-import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
-import SearchBar from "./components/SearchBar/SearchBar";
-import ImageGallery from "./components/ImageGallery/ImageGallery";
-import Loader from "./components/Loader/Loader";
-import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
-import ImageModal from "./components/ImageModal/ImageModal";
-import { getImages } from "./image-api";
-
-export default function App() {
-  const [images, setImages] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [page, setPage] = useState(1);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedImage, setSelectedImage] = useState("");
-
-  useEffect(() => {
-    if (searchQuery.trim() === "") {
-      return;
-    }
-
-    async function fetchImages() {
-      try {
-        setIsLoading(true);
-        setIsError(false);
-        const data = await getImages(searchQuery, page);
-        setImages((prevState) => [...prevState, ...data]);
-      } catch (error) {
-        setIsError(true);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    fetchImages();
-  }, [page, searchQuery]);
-
-  const handleSearch = (topic) => {
-    setSearchQuery(topic);
-    setPage(1);
-    setImages([]);
-  };
-
-  const handleLoadMore = () => {
-    setPage((prevPage) => prevPage + 1);
-  };
-
-  const openModal = (imageUrl) => {
-    setSelectedImage(imageUrl);
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-    setSelectedImage(null);
-  };
-
-  return (
-    <div className="container">
-      <SearchBar onSearch={handleSearch} />
-      {isError && <ErrorMessage />}
-      {images.length > 0 && (
-        <ImageGallery images={images} onImageClick={openModal} />
-      )}
-      {isLoading && <Loader />}
-      {images.length > 0 && !isLoading && (
-        <LoadMoreBtn onClick={handleLoadMore} />
-      )}
-      <ImageModal
-        imageUrl={selectedImage}
-        isOpen={isModalOpen}
-        onClose={closeModal}
-      />
-    </div>
-  );
-}
- */
 import { useState, useEffect } from "react";
 import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
 import SearchBar from "./components/SearchBar/SearchBar";
@@ -93,7 +14,7 @@ export default function App() {
   const [page, setPage] = useState(1);
   const [searchQuery, setSearchQuery] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedImage, setSelectedImage] = useState("");
 
   useEffect(() => {
     if (searchQuery.trim() === "") {
